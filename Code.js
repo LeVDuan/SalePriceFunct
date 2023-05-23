a5af6c3689861e6134566c04f9141898982b3a85
DuanLV <duan.lv0308@gmail.com>
2023-05-24 00:33:04 +0700

    Add comment and Format to VND

--- Code.js
+++ Code.js
@@ -1,17 +1,18 @@
 /**
- * Calculates the sale price of a value at a given discount.
- * The sale price is formatted as US dollars.
+ * Tính giá bán của một giá trị với chiết khấu nhất định.
+ * Giá bán được định dạng là vnd.
  *
- * @param {number} input The value to discount.
- * @param {number} discount The discount to apply, such as .5 or 50%.
- * @return The sale price formatted as USD.
+ * @param {number} input Giá tiền ban đầu.
+ * @param {number} discount Áp dụng giảm giá, chiết khấu, ví dụ .5 hoặc 50%.
+ * @return Thành tiền sau khi đã giảm giá định dạng VND.
  * @customfunction
  */
 function salePrice(input, discount) {
-  let price = input - (input * discount);
-  let dollarUS = Intl.NumberFormat("en-US", {
+  let price = input - (input * discount); // Tính thành tiên sau khi đã có chiết khấu
+  // Điều chỉnh định dạng cho giá trị tiền.
+  let vndVN = Intl.NumberFormat("it-IT", {
     style: "currency",
-    currency: "USD",
+    currency: "VND",
 });
-  return dollarUS.format(price);
+  return vndVN.format(price);
 }
